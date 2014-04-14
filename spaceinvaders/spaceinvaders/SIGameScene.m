89d898cd3c52a8ec674e7872c8acd7ce7626b03a
Henry Chung <Henry.Chung@Desire2Learn.com>
2014-04-14 18:20:01 -0400

    Added a spaceship!

--- spaceinvaders/spaceinvaders/SIGameScene.m
+++ spaceinvaders/spaceinvaders/SIGameScene.m
@@ -20,24 +20,18 @@
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
-        
         _spaceship = [[SISpaceship alloc] initWithImageNamed:@"Spaceship.png"];
-        
     }
     return self;
 }
 
 - (void)didMoveToView:(SKView *)view {
-    
     self.backgroundColor = [SKColor whiteColor];
     self.spaceship.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
     [self addChild:self.spaceship];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
     for (UITouch *touch in touches) {
         
     }
